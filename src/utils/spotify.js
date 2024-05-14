ba557c9b92e6591737eabcf03ade3b486cc273d4
Erik Aakre <erikaakre@hotmail.com>
2024-05-13 20:54:51 -0500

    update for cloud server environment variables

--- src/utils/spotify.js
+++ src/utils/spotify.js
@@ -4,7 +4,7 @@ const baseURL = "https://api.spotify.com/v1";
 
 export const loginURL =
   process.env.NODE_ENV === "production"
-    ? "https://api.mybops.apps.dj/login"
+    ? "api.mybops.apps.dj/login"
     : "http://localhost:3001/login";
 
 function request(url, options) {
